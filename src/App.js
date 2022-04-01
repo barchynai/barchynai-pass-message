dd5b5d2734078259082907ecb065beedc09253dc
barchynai <bar4inay@gmail.com>
2022-04-01 14:31:06 +0600

    pass-message-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import './App.css' 
+
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [prevMessage, setPrevMessage] = useState("");
+  const pass = (e) => {
+    e.preventDefault();
+    if (!message) {
+      return;
+    }
+    setPrevMessage(message);
+    setMessage("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={pass}>
+        <div className="header">
+        <p>A Message You Would Like To Pass</p>
+        <input className="input" value={message} onChange={(e) => setMessage(e.target.value)} />
+        <button type="submit">submit</button>
+        </div>
+        </form>
+        <div className="header">
+        <p>Last Message Delivered</p>
+      <p className="message">{prevMessage}</p>
+        </div>
+     
     </div>
   );
 }
